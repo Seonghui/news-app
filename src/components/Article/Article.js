e02e6b536eaf31d9cbb497d5a4dc1610dc7a5f36
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-10-03 23:03:06 +0900

    Modify modal sass

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Article.scss';
+import { MdClose } from 'react-icons/md';
 
 class Article extends Component {
 
@@ -7,12 +8,18 @@ class Article extends Component {
     const { onClose, photo, head, body } = this.props;
     return (
       <div className="Article">
-        <div className="darkBg"></div>
-        <div className="box">
-          <div className="btn close" onClick={onClose}>닫기</div>
-          {photo && (<img src={photo} alt="newsphoto"></img>)}
-          <h1>{head}</h1>
-          <div dangerouslySetInnerHTML={{ __html: body }} />
+        <div className="darkBg" onClick={ onClose }></div>
+        <div className="news">
+          <div className="news-header">
+            <h1>{head}</h1>
+            <div className="btn close" onClick={ onClose }>
+              <MdClose size={30} />
+            </div>
+          </div>
+          <div className="news-body">
+            {photo && (<img src={photo} alt="newsphoto"></img>)}
+            <div dangerouslySetInnerHTML={{ __html: body }} />
+          </div>
         </div>
       </div>
     );
